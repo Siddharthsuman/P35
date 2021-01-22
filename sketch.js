d0dfc29453ccf3e8c74306089d1d05e988996795
Siddharth <74104002+Siddharthsuman@users.noreply.github.com>
2021-01-22 16:50:04 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -14,47 +14,48 @@ function setup() {
   createCanvas(1365,625);
  balloon=createSprite(160, 420, 10, 10);
  balloon.addImage(balloonImg);
-  balloon.scale=0.8;
+  balloon.scale=0.7;
   //balloon.setCollider("rectangle",0,150,balloon.width,160);
   //
   var balloonPosition=database.ref('balloon/height');
-  balloonPosition.on("value",readHeight,showError);
+  balloonPosition.on("value",readPosition,showError);
   //Creating ground for gravity
-  ground=createSprite(width/2,955,1365,20);
+//  ground=createSprite(width/2,955,1365,20);
 }
 
 function draw() {
   background(backgroundImg);  
-  balloon.velocityY = balloon.velocityY + 0.03;
-  ground.visible=false;
-  if(balloon.isTouching(ground)){
+  //balloon.velocityY = balloon.velocityY + 0.03;
+ // ground.visible=false;
+  /*if(balloon.isTouching(ground)){
     balloon.scale=0.8;
-  }
-  balloon.collide(ground);
+  }*/
+  //balloon.collide(ground);
 //Giving keyCommands
 /*if(keyDown(UP_ARROW)){
-  updateHeight(0,-10);
+ 
   balloon.addAnimation("hotAirBalloon",balloonImg2);
-  balloon.scale=balloon.scale -0.01;
-}*/
- if(keyDown(LEFT_ARROW)){
+  ba*/
+ /*if(keyDown(LEFT_ARROW)){
 
   balloon.x=balloon.x-10;
+  
   balloon.addImage(balloonImg2);
  }
 else if(keyDown(RIGHT_ARROW)){
   balloon.x=balloon.x+10;
   balloon.addImage(balloonImg3);
 }
-else if(keyDown(UP_ARROW)){
-  balloon.y=balloon.y-10;
+else*/ if(keyDown(UP_ARROW)){
+ // balloon.y=balloon.y-10;
+  updateHeight(0,-1);
   balloon.scale=balloon.scale -0.01;
 
 }
-else if(keyDown(DOWN_ARROW)){
+/*else if(keyDown(DOWN_ARROW)){
   balloon.y=balloon.y+10;
   balloon.scale=balloon.scale +0.01;
-}
+}*/
   drawSprites();
 
 
@@ -75,4 +76,9 @@ function readHeight(data){
 
 function showError(){
   console.log("Error in writing to the database");
+}
+function readPosition(data){
+  position = data.val();
+  balloon.x = position.x;
+  balloon.y = position.y
 }
